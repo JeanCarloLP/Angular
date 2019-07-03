d4ed1aac41b57156f80b61be79c7857d28a2ae60
The Chan <jeancarloleonpr@gmail.com>
2019-07-03 23:13:14 +0200

    APP-1 Instalación de Bootstrap
    instalación por npm o scripts en el index

--- Angular/001_Spa/src/app/app.module.ts
+++ Angular/001_Spa/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular/001_Spa/src/app/components/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styles: []
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
