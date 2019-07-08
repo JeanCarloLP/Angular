ff76cd339b04a4c05ed6e05a31a53014f5c01c97
The Chan <jeancarloleonpr@gmail.com>
2019-07-08 22:09:49 +0200

    APP-001 Services
    uso de servicios y ngfor

--- Angular/001_Spa/src/app/components/heroes/heroes.component.ts
+++ Angular/001_Spa/src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styles: []
+  styles: [ ]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  aHeroes: Heroe[] = [];
+
+  constructor( private heroesService: HeroesService ) {
+    console.log('constructor');
+  }
 
   ngOnInit() {
+    this.aHeroes = this.heroesService.getHeroes();
+    console.log(this.aHeroes);
   }
 
 }
