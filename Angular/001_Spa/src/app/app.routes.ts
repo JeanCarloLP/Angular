0a482ebbb3dcdcf0811d3bbf42f19c755cd38876
The Chan <jeancarloleonpr@gmail.com>
2019-07-09 21:02:01 +0200

    JCL 09/07/2019 APP-001
    Uso de ActivatedRoute observable de parametros en la url

--- Angular/001_Spa/src/app/app.routes.ts
+++ Angular/001_Spa/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { AboutComponent } from './components/about/about.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 const APP_ROUTES: Routes = [
     { path: 'about', component: AboutComponent },
+    { path: 'heroe/:id', component: HeroeComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'home', component: HomeComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
--- Angular/001_Spa/src/app/components/heroes/heroes.component.ts
+++ Angular/001_Spa/src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,13 +10,18 @@ export class HeroesComponent implements OnInit {
 
   aHeroes: Heroe[] = [];
 
-  constructor( private heroesService: HeroesService ) {
-    console.log('constructor');
+  constructor( private heroesService: HeroesService,
+               private router: Router ) {
+    // console.log('constructor');
   }
 
   ngOnInit() {
     this.aHeroes = this.heroesService.getHeroes();
-    console.log(this.aHeroes);
+    // console.log(this.aHeroes);
+  }
+
+  verHeroe( idx: number) {
+    this.router.navigate( ['/heroe', idx] );
   }
 
 }
