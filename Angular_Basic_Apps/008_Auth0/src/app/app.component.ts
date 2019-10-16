ed9b5b225f8ec8364f513629209cf0598b1a2ccd
The Chan <jeancarloleonpr@gmail.com>
2019-10-16 16:14:52 +0200

    Auth 0 app with Angular 8
    Initial config to set the Auth0 service to use social media as a way to log in

--- Angular_Basic_Apps/008_Auth0/src/app/app.component.ts
+++ Angular_Basic_Apps/008_Auth0/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'auth';
+export class AppComponent implements OnInit {
+  constructor( private auth: AuthService ) {}
+
+  ngOnInit(): void {
+    this.auth.localAuthSetup();
+  }
 }
