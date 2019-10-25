130629b9038e875c770a0cdeff9ea92477f83ef5
The Chan <jeancarloleonpr@gmail.com>
2019-10-25 13:24:42 +0200

    JCL template updated

--- Angular_Basic_Apps/010_Form_Data/src/app/app.module.ts
+++ Angular_Basic_Apps/010_Form_Data/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DataComponent } from './components/data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
